240677c61250e9069870da41edbcf8b5abed6558
Nikola Lakic <nikola.lakic@comdata.rs>
2021-03-18 13:14:30 +0100

    initial commit

---
+++ apps/e-pub-book/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'e-pub-book-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'e-pub-book';
+}
